ee4c383c80dd70fb439d9eae21a104439f2417fc
makame <makame@mail.ru>
2020-05-28 14:35:50 +0200

    clean csw

--- src/custom-service-worker.js
+++ src/custom-service-worker.js
@@ -1,6 +1,4 @@
 /* eslint-disable no-restricted-globals */
-import signalhub from "signalhub";
-import crypto from "crypto";
 import localforage from "localforage";
 
 localforage.config({
@@ -8,14 +6,9 @@ localforage.config({
 });
 
 let channels = [];
-let hubs = [];
-
-// init();
 
 async function init() {
   channels = JSON.parse((await localforage.getItem("channels")) || "[]");
-  hubs.forEach((hub) => hub.close());
-  hubs = channels.map(connectHub);
 }
 
 async function updateClientChannels() {
@@ -53,32 +46,16 @@ addEventListener("message", (event) => {
   );
 });
 
-function connectHub(name) {
-  const hub = signalhub(getTopic(name), [
-    "https://signalhub-hzbibrznqa.now.sh",
-    // "https://signalhub-jccqtwhdwc.now.sh",
-  ]);
-
-  hub.subscribe(getTopic(name)).on("data", (message) => {
-    onHubMessage(name, message);
-  });
-
-  return hub;
-}
-
 async function addChannel(name) {
   if (!channels.includes(name)) {
     channels.push(name);
     await localforage.setItem("channels", JSON.stringify(channels));
-    hubs.push(connectHub(name));
   }
 }
 
 async function removeChannel(name) {
   const index = channels.indexOf(name);
   if (index >= 0) {
-    channels.splice(index, 1);
-    hubs[index].close();
     channels.splice(index, 1);
     await localforage.setItem("channels", JSON.stringify(channels));
   }
@@ -87,71 +64,6 @@ async function removeChannel(name) {
 function getChannels() {
   return channels;
 }
-
-function getTopic(name) {
-  return crypto
-    .createHash("sha256")
-    .update("HYPERCHAT_" + name)
-    .digest()
-    .toString("hex");
-}
-
-function getRoomNameFromUrl(url) {
-  url = new URL(url);
-  const arr = /^\/room\/(.+)$/gi.exec(url.pathname);
-  return arr && arr[1];
-}
-
-async function onHubMessage(name, data) {
-  if (Notification.permission && data.type === "connect" && data.id) {
-    await new Promise((r) => setTimeout(r, 1000));
-    const clients = await self.clients.matchAll();
-    const existedRoom = clients.find((client) => {
-      return getRoomNameFromUrl(client.url) === name;
-    });
-
-    if (existedRoom) {
-      return;
-    }
-
-    await self.registration.showNotification(
-      `HVC: [${data.username || "unknown"}] has joined "${name}" room`,
-      {
-        actions: [{ action: "open_url", title: "Open Now" }],
-        data: { name },
-        vibrate: [200, 100, 200, 100, 200, 100, 200],
-        requireInteraction: true,
-        body: `"${name}" has got a new connection with a username: ${data.username ||
-          "unknown"}`,
-        icon: "./logo192.png",
-      }
-    );
-  }
-}
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close(); // Android needs explicit close.
-  if (event.action === "open_url") {
-    event.waitUntil(
-      self.clients.matchAll({ type: "window" }).then((windowClients) => {
-        for (let i = 0; i < windowClients.length; i++) {
-          const client = windowClients[i];
-          // If so, just focus it.
-          const name = getRoomNameFromUrl(client.url);
-          if (name === event.notification.data.name && "focus" in client) {
-            return client.focus();
-          }
-        }
-        // If not, then open the target URL in a new window/tab.
-        if (self.clients.openWindow) {
-          return self.clients.openWindow(
-            "https://hypervideochat.now.sh/room/" + event.notification.data.name
-          );
-        }
-      })
-    );
-  }
-});
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
